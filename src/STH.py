c6c0e5d44e811facc155f8a520a425c356da5612
Arnav Badrish <arnavbadrish@gmail.com>
2023-08-28 18:13:09 +1000

    Created new class Vision to 'see' the screen

--- src/STH.py
+++ src/STH.py
@@ -1,4 +1,7 @@
 from pynput.mouse import Button, Controller as MouseController
+from mss import mss
+import cv2
+import numpy as np
 
 class Controller:
     def __init__(self):
@@ -10,3 +13,32 @@ class Controller:
     def left_click(self):
         self.mouse.click(Button.left)
 
+class Vision:
+    def __init__(self):
+        self.template_paths = {
+            "OperationComplete": "assets/OperationComplete.png",
+            "RepeatButton": "assets/RepeatButton.png"
+        }
+        #self.templates = {k: cv2.imread(v, 0) for k, v in self.template_paths.items()}
+        self.monitor = {'top': 0, 'left': 0, 'width': 1920, 'height': 1080}
+        self.screen = mss()
+
+    def take_screenshot(self):
+        ss = self.screen.grab(self.monitor)
+        img = np.array(ss)
+        img_gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+        return img_gray
+    
+    def match_template(self, img_gray, template, threshold):
+        res = cv2.matchTemplate(img_gray, template, cv2.TM_CCOEFF_NORMED)
+        loc = np.where(res >= threshold)
+        return loc
+
+    def find_template(self, name, image=None):
+        threshold = 0.6
+        if image is None:
+            image = self.take_screenshot()
+        return self.match_template(image, self.templates[name], threshold)
+    
+m = Vision()
+img = m.take_screenshot()
